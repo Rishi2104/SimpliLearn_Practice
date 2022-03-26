dfb1d29a6150c70643315e5ac2b3b7621f3dd789
Rishi <rishi8155@gmail.com>
2022-03-26 16:46:03 +0530

    Improvement 02

--- TypeCasting.java
+++ TypeCasting.java
@@ -2,12 +2,12 @@ class Tcast {
 	void implicit(char val) {
 		long v1=val;
 		double v2=val;
-		System.out.println("Long = "+v1+"\nDouble = "+v2);
+		System.out.println("\t\tImplicit:\nLong = "+v1+"\nDouble = "+v2);
 	}
 	void explicit(double k) {
 		double k1=k;
 		int p=(int)(k1);
-		System.out.println("\nDouble to Integer \n"+ k+" to "+p);
+		System.out.println("\t\tExplicit:\nDouble to Integer \n"+ k+" to "+p);
 	}
 }
 public class TypeCasting {
